13b7eca50097d5fe76b77de7df9f5fe55d95bc6a
adamShiau <69100634+adamShiau@users.noreply.github.com>
2020-11-25 18:00:58 +0800

    updata BT

---
+++ HC-05_AT/HC-05_AT.ino
@@ -0,0 +1,20 @@
+char val;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial1.begin(38400);
+  Serial.begin(38400);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(Serial.available()) {
+    val = Serial.read();
+    Serial1.print(val);
+  }
+
+  if(Serial1.available()) {
+    val = Serial1.read();
+    Serial.print(val);
+  }
+}
--- nano_IMU_BT/nano_IMU_BT.ino
+++ nano_IMU_BT/nano_IMU_BT.ino
@@ -4,39 +4,40 @@
 
 #define PRINT_GYRO 0
 #define PRINT_XLM 0
+#define PRINT_TIME 0
 #define PERIOD 10000
 
-bool clk_status = 0;
+bool clk_status = 1;
 unsigned long start_time = 0;
 
-Uart mySerial5 (&sercom0, 5, 6, SERCOM_RX_PAD_1, UART_TX_PAD_0);
-Uart mySerial13 (&sercom1, 13, 8, SERCOM_RX_PAD_1, UART_TX_PAD_2);
+//Uart mySerial5 (&sercom0, 5, 6, SERCOM_RX_PAD_1, UART_TX_PAD_0);
+//Uart mySerial13 (&sercom1, 13, 8, SERCOM_RX_PAD_1, UART_TX_PAD_2);
 
 // Attach the interrupt handler to the SERCOM
-void SERCOM0_Handler()
-{
-    mySerial5.IrqHandler();
-}
-void SERCOM1_Handler()
-{
-    mySerial13.IrqHandler();
-}
+//void SERCOM0_Handler()
+//{
+//    mySerial5.IrqHandler();
+//}
+//void SERCOM1_Handler()
+//{
+//    mySerial13.IrqHandler();
+//}
 
-  pinPeripheral(5, PIO_SERCOM_ALT); //RX
-  pinPeripheral(6, PIO_SERCOM_ALT); //TX
-    // Reassign pins 13 and 8 to SERCOM (not alt this time)
-  pinPeripheral(13, PIO_SERCOM);
-  pinPeripheral(8, PIO_SERCOM);
-  
-  mySerial5.begin(115200); //rx:p5, tx:p6
-  mySerial13.begin(115200);//rx:p13, tx:p8
-  Serial1.begin(115200);//rx:p0, tx:p1
-  Serial.begin(115200);// buit in
+
 
 void setup() {
+  // Reassign pins 5 and 6 to SERCOM alt
+//  pinPeripheral(5, PIO_SERCOM_ALT); //RX
+//  pinPeripheral(6, PIO_SERCOM_ALT); //TX
+  // Reassign pins 13 and 8 to SERCOM (not alt this time)
+//  pinPeripheral(13, PIO_SERCOM);
+//  pinPeripheral(8, PIO_SERCOM);
+
+//  mySerial5.begin(115200); //rx:p5, tx:p6
+//  mySerial13.begin(115200);//rx:p13, tx:p8
   Serial.begin(115200);
   Serial1.begin(115200); //for HC-05
-  while (!Serial);
+//  while (!Serial);
   if (!IMU.begin()) {
     while (1);
   }
@@ -49,11 +50,24 @@ void loop() {
     checkByte(0xAA);
     request_xlm(ax, ay, az);
     request_gyro(wx, wy, wz);
+    send_current_time(start_time);
+    start_time = micros();
 //    Serial.println(millis());
   }
   output_fogClk(start_time);
 }
 
+void send_current_time(unsigned long current_time) {
+  if(PRINT_TIME) {
+    Serial.print("t");
+    Serial.print('\t');
+    Serial.println(current_time);
+  }
+  Serial1.write(current_time>>24);
+  Serial1.write(current_time>>16);
+  Serial1.write(current_time>>8);
+  Serial1.write(current_time);
+}
 
 void request_xlm(int x, int y, int z) {
   while (!IMU.accelerationAvailable()); 
@@ -97,7 +111,7 @@ void request_gyro(int x, int y, int z) {
 
 void output_fogClk(unsigned long tin) {
   if((micros()-tin)>=PERIOD) {
-    start_time = micros();
+//    start_time = micros();
     clk_status = 1;
   }
 }
