ec565512f8c886d62619bab50685ea153350bf6b
adam1025 <69100634+adamShiau@users.noreply.github.com>
2022-09-18 13:32:26 +0800

    update

--- EIC_test/EIC_test.ino
+++ EIC_test/EIC_test.ino
@@ -1,43 +1,51 @@
 volatile int cnt = 0;
+#include "pig_v2.h"
 //byte tt=0;
+#define TRIG_OUT 3
+
+PIG pig_ser1(Serial1);
 
 void setup() {
   // put your setup code here, to run once:
-//  pinMode(2, INPUT_PULLUP);
-  attachInterrupt(2, ISR_test, RISING);
-  pinMode(3, OUTPUT);
 
+  attachInterrupt(2, ISR_test, RISING);
+  pinMode(TRIG_OUT, OUTPUT);
+  digitalWrite(TRIG_OUT, LOW);
+  Serial.begin(230400);
+  Serial1.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-//Serial.println("hi");
-//delay(1000);
-//Serial.println(EIC->INTFLAG.reg, HEX);
+byte header_ser1[2];
+byte fog_ser1[14];
+  
+pig_ser1.readData(header_ser1, fog_ser1);
+Serial1.write(fog_ser1, 14);
+
+//digitalWrite(TRIG_OUT, LOW);
+EIC->CONFIG[1].reg = 0x100;
+
+
 }
+
 
 void ISR_test()
 {
   EIC->CONFIG[1].reg = 0;
-  do_somthing();
-//  noInterrupts();
-  Serial.println(g_APinDescription[2].ulExtInt);
-  Serial.println(EIC->CONFIG[0].reg, HEX);
-  Serial.println(EIC->CONFIG[1].reg, HEX);
-  Serial.println(cnt);
-  Serial.println();
-  
-  cnt++;
+  digitalWrite(TRIG_OUT, HIGH);
+  loopdelay(10);
+  digitalWrite(TRIG_OUT, LOW);
+//  EIC->CONFIG[1].reg = 0x100;
 }
 
-void do_somthing()
+void loopdelay(int dly)
 {
 //  flag = 0;
   
-  for(int i=0; i<65535; i++) {
+  for(int i=0; i<dly; i++) {
     Serial.println(i);
   }
-  EIC->CONFIG[1].reg = 0x100;
+//  EIC->CONFIG[1].reg = 0x100;
 //  delay(10);
 //  flag = 1;
 }
