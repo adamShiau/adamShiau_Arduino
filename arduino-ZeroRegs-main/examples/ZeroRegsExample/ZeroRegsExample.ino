bcbcadb08781bfa8634d420655a9ad46507f2d1b
adamShiau <ami731025@gmail.com>
2022-12-26 18:21:38 +0800

    uu

--- arduino-ZeroRegs-main/examples/ZeroRegsExample/ZeroRegsExample.ino
+++ arduino-ZeroRegs-main/examples/ZeroRegsExample/ZeroRegsExample.ino
@@ -5,6 +5,16 @@ void setup() {
   while (! Serial) {}  // wait for serial monitor to attach
   ZeroRegOptions opts = { Serial, false };
   printZeroRegs(opts);
+  Serial.println("HIHIHIHI");
+  Serial.println(SYSCTRL->OSC32K.bit.ENABLE);
+  Serial.println(SYSCTRL->OSC32K.bit.EN32K);
+  Serial.println(SYSCTRL->OSC32K.bit.EN1K);
+  Serial.println(SYSCTRL->OSC32K.bit.RUNSTDBY);
+  Serial.println(SYSCTRL->OSC32K.bit.ONDEMAND);
+  Serial.println(SYSCTRL->OSC32K.bit.STARTUP);
+  Serial.println(SYSCTRL->OSC32K.bit.WRTLOCK);
+  Serial.println(SYSCTRL->OSC32K.bit.CALIB);
+  Serial.println(SYSCTRL->OSC32K.reg, HEX);
 }
 
 void loop() {
