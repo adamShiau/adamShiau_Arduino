734740ba0d3c3153b6831d861d6e8b84b10eb54d
adam1025 <69100634+adamShiau@users.noreply.github.com>
2023-04-18 23:50:01 +0800

    up

--- libraries/uartRT/example/test/test.ino
+++ libraries/uartRT/example/test/test.ino
@@ -3,6 +3,8 @@
 
 uartRT myUart(Serial1);
 uint8_t trailer = 0x55;
+uint8_t header[2] = {0xAB, 0xBA};
+uint16_t try_cnt=0;
 
 void setup() {
     Serial.begin(230400);
@@ -14,12 +16,9 @@ void loop() {
     // myUart.readData();
   byte *data;
 
+/****
   data = myUart.readData(&trailer);
-  // Serial.print(" data: ");
-  // Serial.println((long) data);
   if(data) {
-  // uint8_t ss =  sizeof(data);
-    // Serial.println(sizeof(data));
   Serial.println("\nreceived data: ");
     for(int i=0; i<sizeof(data); i++) {
       Serial.print(data[i], HEX);
@@ -27,5 +26,19 @@ void loop() {
      }
      Serial.println("");
   }
+***/
+  data = myUart.readData(header, &trailer, &try_cnt);
+  if(data) {
+  Serial.println("\nreceived data: ");
+    for(int i=0; i<sizeof(data); i++) {
+      Serial.print(try_cnt);
+      Serial.print(", ");
+      Serial.print(data[i], HEX);
+      Serial.print(" ");
+     }
+     Serial.println("");
+  }
+
+
 
 }
