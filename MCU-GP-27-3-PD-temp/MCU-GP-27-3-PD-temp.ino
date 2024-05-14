b5c3998ecee7461d1447548e6e0501aa05fef5c4
adamShiau <ami731025@gmail.com>
2024-05-14 19:54:18 +0800

    d

--- MCU-GP-27-3-PD-temp/MCU-GP-27-3-PD-temp.ino
+++ MCU-GP-27-3-PD-temp/MCU-GP-27-3-PD-temp.ino
@@ -78,10 +78,10 @@ volatile bool ISR_PEDGE;
 
 int t_adc = millis();
 
-/*** * Watch dog  * **/
-static void   WDTsync() {
-  while (WDT->STATUS.bit.SYNCBUSY == 1); //Just wait till WDT is free
-}
+// /*** * Watch dog  * **/
+// static void   WDTsync() {
+//   while (WDT->STATUS.bit.SYNCBUSY == 1); //Just wait till WDT is free
+// }
 int WDT_CNT=0;
 int tt0=0, tt1, tt2, tt3;
 unsigned long data_cnt = 0;
@@ -98,9 +98,13 @@ unsigned int t_previous = 0;
 
 
 void setup() {
+
+  // XOSC32K_SET();
+  XOSC32K_CLK_SET();
+
   /*** pwm ***/
     pwm_init();
- 
+    myWDT_init(); //disable all WDT
   
     // EXTT
     /*** for IMU_V4  : EXTT = PA27, Variant pin = 26, EXINT[15]
@@ -108,7 +112,7 @@ void setup() {
      *  ****/
   attachInterrupt(26, ISR_EXTT, CHANGE);
 
-  disableWDT();
+  // disableWDT();
 // set interrupt mode to None
   /***----- for PIG MCU & IMU_V4 EXINT[15]----- ***/
   EIC->CONFIG[1].bit.SENSE7 = 0;  // set ISR no NONE
@@ -150,7 +154,7 @@ void setup() {
 	/*** var initialization***/
 	cmd_complete = 0;
 	mux_flag = MUX_ESCAPE; 		//default set mux_flag to 2
-	select_fn = SEL_DEFAULT; 	//default set select_fn to 128
+	select_fn = SEL_DEFAULT; 	
 	// select_fn = SEL_IMU;
 	run_fog_flag = 0;
 	output_fn = temp_idle;
@@ -736,6 +740,8 @@ void acq_fog_parameter(byte &select_fn, unsigned int value, byte ch)
         EIC->CONFIG[1].bit.SENSE7 = 0; //set interrupt condition to None
         eeprom.Write(EEPROM_ADDR_FOG_STATUS, 1);
         setupWDT(11);
+        enable_EXT_WDT(EXT_WDT_EN);
+        reset_EXT_WDI(WDI);
       break;
 
       case EXT_SYNC:
@@ -745,6 +751,8 @@ void acq_fog_parameter(byte &select_fn, unsigned int value, byte ch)
         EIC->CONFIG[1].bit.SENSE7 = 3; ////set interrupt condition to Both
         eeprom.Write(EEPROM_ADDR_FOG_STATUS, 1);
         setupWDT(11);
+        enable_EXT_WDT(EXT_WDT_EN);
+        reset_EXT_WDI(WDI);
       break;
 
       case STOP_SYNC:
@@ -753,6 +761,7 @@ void acq_fog_parameter(byte &select_fn, unsigned int value, byte ch)
         EIC->CONFIG[1].bit.SENSE7 = 0; //set interrupt condition to None
         eeprom.Write(EEPROM_ADDR_FOG_STATUS, 0);
         disableWDT();
+        disable_EXT_WDT(EXT_WDT_EN);
       break;
 
       default:
@@ -791,7 +800,8 @@ void acq_fog_parameter(byte &select_fn, unsigned int value, byte ch)
           Serial1.write(CRC32, 4);
         }
        #endif
-      resetWDT();
+      resetWDT(); 
+      reset_EXT_WDI(WDI);
       }
       t_old = t_new;   
 	}
@@ -829,7 +839,9 @@ void acq_fog(byte &select_fn, unsigned int value, byte ch)
         data_cnt = 0;
         EIC->CONFIG[1].bit.SENSE7 = 3; ////set interrupt condition to Both
         eeprom.Write(EEPROM_ADDR_FOG_STATUS, 1);
-        // setupWDT(11);
+        setupWDT(11);
+        enable_EXT_WDT(EXT_WDT_EN);
+        reset_EXT_WDI(WDI);
       break;
 
       case STOP_SYNC:
@@ -838,6 +850,7 @@ void acq_fog(byte &select_fn, unsigned int value, byte ch)
         EIC->CONFIG[1].bit.SENSE7 = 0; //set interrupt condition to None
         eeprom.Write(EEPROM_ADDR_FOG_STATUS, 0);
         disableWDT();
+        disable_EXT_WDT(EXT_WDT_EN);
       break;
 
       default:
@@ -883,6 +896,7 @@ void acq_fog(byte &select_fn, unsigned int value, byte ch)
         #endif
         }
         resetWDT();
+        reset_EXT_WDI(WDI);
       }
       t_old = t_new;  
 	}
@@ -914,6 +928,8 @@ void acq_imu(byte &select_fn, unsigned int value, byte ch)
         EIC->CONFIG[1].bit.SENSE7 = 0; //set interrupt condition to None
         eeprom.Write(EEPROM_ADDR_FOG_STATUS, 1);
         setupWDT(11);
+        enable_EXT_WDT(EXT_WDT_EN);
+        reset_EXT_WDI(WDI);
         
       break;
       case EXT_SYNC:
@@ -924,6 +940,8 @@ void acq_imu(byte &select_fn, unsigned int value, byte ch)
         EIC->CONFIG[1].bit.SENSE7 = 3; ////set interrupt condition to Both
         eeprom.Write(EEPROM_ADDR_FOG_STATUS, 1);
         setupWDT(11);
+        enable_EXT_WDT(EXT_WDT_EN);
+        reset_EXT_WDI(WDI);
 
       break;
       case STOP_SYNC:
@@ -932,6 +950,7 @@ void acq_imu(byte &select_fn, unsigned int value, byte ch)
         EIC->CONFIG[1].bit.SENSE7 = 0; //set interrupt condition to None
         eeprom.Write(EEPROM_ADDR_FOG_STATUS, 0);
         disableWDT();
+        disable_EXT_WDT(EXT_WDT_EN);
       break;
 
       default:
@@ -983,6 +1002,7 @@ void acq_imu(byte &select_fn, unsigned int value, byte ch)
       }
       #endif  
       resetWDT(); 
+      reset_EXT_WDI(WDI);
     }
     t_old = t_new;    
 	}
@@ -1013,6 +1033,8 @@ void acq_nmea(byte &select_fn, unsigned int value, byte ch)
         EIC->CONFIG[1].bit.SENSE7 = 0; //set interrupt condition to None
         eeprom.Write(EEPROM_ADDR_FOG_STATUS, 1);
         setupWDT(11);
+        enable_EXT_WDT(EXT_WDT_EN);
+        reset_EXT_WDI(WDI);
       break;
 
       case EXT_SYNC:
@@ -1022,6 +1044,8 @@ void acq_nmea(byte &select_fn, unsigned int value, byte ch)
         EIC->CONFIG[1].bit.SENSE7 = 3; ////set interrupt condition to Both
         eeprom.Write(EEPROM_ADDR_FOG_STATUS, 1);
         setupWDT(11);
+        enable_EXT_WDT(EXT_WDT_EN);
+        reset_EXT_WDI(WDI);
       break;
 
       case NMEA_MODE:
@@ -1031,6 +1055,8 @@ void acq_nmea(byte &select_fn, unsigned int value, byte ch)
         EIC->CONFIG[1].bit.SENSE7 = 3; ////set interrupt condition to Both
         eeprom.Write(EEPROM_ADDR_FOG_STATUS, 1);
         setupWDT(11);
+        enable_EXT_WDT(EXT_WDT_EN);
+        reset_EXT_WDI(WDI);
       break;
 
       case STOP_SYNC:
@@ -1039,6 +1065,7 @@ void acq_nmea(byte &select_fn, unsigned int value, byte ch)
         eeprom.Write(EEPROM_ADDR_FOG_STATUS, 0);
         data_cnt = 0;
         disableWDT();
+        disable_EXT_WDT(EXT_WDT_EN);
       break;
 
       default:
@@ -1068,6 +1095,7 @@ void acq_nmea(byte &select_fn, unsigned int value, byte ch)
         
        #endif 
        resetWDT();
+       reset_EXT_WDI(WDI);
       }
 	}
 	clear_SEL_EN(select_fn);	
@@ -1294,44 +1322,6 @@ void readADC(byte data[8])
   // Serial.print(" ");
   // Serial.println(my_f.bin_val[0], HEX);
   
-}
-
-//============= resetWDT ===================================================== 
-void resetWDT() {
-  // reset the WDT watchdog timer.
-  // this must be called before the WDT resets the system
-  WDT->CLEAR.reg= 0xA5; // reset the WDT
-  WDTsync(); 
-  // Serial.println("resetWDT");
-}
-
-//============= systemReset ================================================== 
-void systemReset() {
-  // use the WDT watchdog timer to force a system reset.
-  // WDT MUST be running for this to work
-  WDT->CLEAR.reg= 0x00; // system reset via WDT
-  WDTsync(); 
-}
-
-//============= setupWDT =====================================================
-void setupWDT( uint8_t period) {
-  // initialize the WDT watchdog timer
-
-  WDT->CTRL.reg = 0; // disable watchdog
-  WDTsync(); // sync is required
-
-  WDT->CONFIG.reg = min(period,11); // see Table 17-5 Timeout Period (valid values 0-11)
-
-  WDT->CTRL.reg = WDT_CTRL_ENABLE; //enable watchdog
-  WDTsync(); 
-  Serial.println("setupWDT");
-}
-
-//============= disable WDT =====================================================
-void disableWDT() {
-  WDT->CTRL.reg = 0; // disable watchdog
-  WDTsync(); // sync is required
-  Serial.println("disableWDT");
 }
 
 void parameter_init(void)
@@ -1796,3 +1786,187 @@ void reset_SYNC()
     Serial.print("Version:");
     Serial.println(MCU_VERSION);
   }
+
+  void XOSC32K_SET()
+{
+  /* Set the correct number of wait states for 48 MHz @ 3.3v */
+NVMCTRL->CTRLB.bit.RWS = 1;
+SYSCTRL->XOSC32K.reg =
+  /* Crystal oscillators can take a long time to startup. This
+      waits the maximum amount of time (4 seconds). This can be
+      reduced depending on your crystal oscillator. */
+  SYSCTRL_XOSC32K_STARTUP(0x5) |
+  SYSCTRL_XOSC32K_EN32K |
+  SYSCTRL_XOSC32K_XTALEN;
+
+/* This has to be a separate write as per datasheet section 17.6.3 */
+SYSCTRL->XOSC32K.bit.ENABLE = 1;
+
+/* Wait for the external crystal to be ready */
+while(!SYSCTRL->PCLKSR.bit.XOSC32KRDY);
+/* Configure GCLK1's divider - in this case, no division - so just divide by one */
+GCLK->GENDIV.reg =
+    GCLK_GENDIV_ID(1) |
+    GCLK_GENDIV_DIV(1);
+
+/* Setup GCLK1 using the external 32.768 kHz oscillator */
+GCLK->GENCTRL.reg =
+GCLK_GENCTRL_ID(1) |
+GCLK_GENCTRL_SRC_XOSC32K |
+/* Improve the duty cycle. */
+GCLK_GENCTRL_IDC |
+GCLK_GENCTRL_GENEN;
+
+/* Wait for the write to complete */
+while(GCLK->STATUS.bit.SYNCBUSY);
+
+GCLK->CLKCTRL.reg =
+    GCLK_CLKCTRL_ID_DFLL48 |
+    GCLK_CLKCTRL_GEN_GCLK1 |
+    GCLK_CLKCTRL_CLKEN;
+
+/* This works around a quirk in the hardware (errata 1.2.1) -
+   the DFLLCTRL register must be manually reset to this value before
+   configuration. */
+while(!SYSCTRL->PCLKSR.bit.DFLLRDY);
+SYSCTRL->DFLLCTRL.reg = SYSCTRL_DFLLCTRL_ENABLE;
+while(!SYSCTRL->PCLKSR.bit.DFLLRDY);
+
+/* Set up the multiplier. This tells the DFLL to multiply the 32.768 kHz
+   reference clock to 48 MHz */
+SYSCTRL->DFLLMUL.reg =
+    /* This value is output frequency / reference clock frequency,
+       so 48 MHz / 32.768 kHz = 1465*/
+    SYSCTRL_DFLLMUL_MUL(1465) |
+    /* The coarse and fine step are used by the DFLL to lock
+       on to the target frequency. These are set to half
+       of the maximum value. Lower values mean less overshoot,
+       whereas higher values typically result in some overshoot but
+       faster locking. */
+    SYSCTRL_DFLLMUL_FSTEP(511) | // max value: 1023
+    SYSCTRL_DFLLMUL_CSTEP(31);  // max value: 63
+
+/* Wait for the write to finish */
+while(!SYSCTRL->PCLKSR.bit.DFLLRDY);
+
+uint32_t coarse = (*((uint32_t *)FUSES_DFLL48M_COARSE_CAL_ADDR) & FUSES_DFLL48M_COARSE_CAL_Msk) >> FUSES_DFLL48M_COARSE_CAL_Pos;
+
+SYSCTRL->DFLLVAL.bit.COARSE = coarse;
+
+/* Wait for the write to finish */
+while(!SYSCTRL->PCLKSR.bit.DFLLRDY);
+
+SYSCTRL->DFLLCTRL.reg |=
+    /* Closed loop mode */
+    SYSCTRL_DFLLCTRL_MODE |
+    /* Wait for the frequency to be locked before outputting the clock */
+    SYSCTRL_DFLLCTRL_WAITLOCK |
+    /* Enable it */
+    SYSCTRL_DFLLCTRL_ENABLE;
+
+/* Wait for the frequency to lock */
+while (!SYSCTRL->PCLKSR.bit.DFLLLCKC || !SYSCTRL->PCLKSR.bit.DFLLLCKF) {}
+
+/* Setup GCLK0 using the DFLL @ 48 MHz */
+GCLK->GENCTRL.reg =
+    GCLK_GENCTRL_ID(0) |
+    GCLK_GENCTRL_SRC_DFLL48M |
+    /* Improve the duty cycle. */
+    GCLK_GENCTRL_IDC |
+    GCLK_GENCTRL_GENEN;
+
+/* Wait for the write to complete */
+while(GCLK->STATUS.bit.SYNCBUSY);
+}
+
+void XOSC32K_CLK_SET()
+{
+  /* Set the correct number of wait states for 48 MHz @ 3.3v */
+NVMCTRL->CTRLB.bit.RWS = 1;
+SYSCTRL->XOSC32K.reg =
+  /* Crystal oscillators can take a long time to startup. This
+      waits the maximum amount of time (4 seconds). This can be
+      reduced depending on your crystal oscillator. */
+  SYSCTRL_XOSC32K_STARTUP(0x5) |
+  SYSCTRL_XOSC32K_EN32K;
+
+/* This has to be a separate write as per datasheet section 17.6.3 */
+SYSCTRL->XOSC32K.bit.ENABLE = 1;
+
+/* Wait for the external crystal to be ready */
+while(!SYSCTRL->PCLKSR.bit.XOSC32KRDY);
+/* Configure GCLK1's divider - in this case, no division - so just divide by one */
+GCLK->GENDIV.reg =
+    GCLK_GENDIV_ID(1) |
+    GCLK_GENDIV_DIV(1);
+
+/* Setup GCLK1 using the external 32.768 kHz oscillator */
+GCLK->GENCTRL.reg =
+GCLK_GENCTRL_ID(1) |
+GCLK_GENCTRL_SRC_XOSC32K |
+/* Improve the duty cycle. */
+GCLK_GENCTRL_IDC |
+GCLK_GENCTRL_GENEN;
+
+/* Wait for the write to complete */
+while(GCLK->STATUS.bit.SYNCBUSY);
+
+GCLK->CLKCTRL.reg =
+    GCLK_CLKCTRL_ID_DFLL48 |
+    GCLK_CLKCTRL_GEN_GCLK1 |
+    GCLK_CLKCTRL_CLKEN;
+
+/* This works around a quirk in the hardware (errata 1.2.1) -
+   the DFLLCTRL register must be manually reset to this value before
+   configuration. */
+while(!SYSCTRL->PCLKSR.bit.DFLLRDY);
+SYSCTRL->DFLLCTRL.reg = SYSCTRL_DFLLCTRL_ENABLE;
+while(!SYSCTRL->PCLKSR.bit.DFLLRDY);
+
+/* Set up the multiplier. This tells the DFLL to multiply the 32.768 kHz
+   reference clock to 48 MHz */
+SYSCTRL->DFLLMUL.reg =
+    /* This value is output frequency / reference clock frequency,
+       so 48 MHz / 32.768 kHz = 1465*/
+    // SYSCTRL_DFLLMUL_MUL(48000) |
+    SYSCTRL_DFLLMUL_MUL(1465) |
+    /* The coarse and fine step are used by the DFLL to lock
+       on to the target frequency. These are set to half
+       of the maximum value. Lower values mean less overshoot,
+       whereas higher values typically result in some overshoot but
+       faster locking. */
+    SYSCTRL_DFLLMUL_FSTEP(511) | // max value: 1023
+    SYSCTRL_DFLLMUL_CSTEP(31);  // max value: 63
+
+/* Wait for the write to finish */
+while(!SYSCTRL->PCLKSR.bit.DFLLRDY);
+
+uint32_t coarse = (*((uint32_t *)FUSES_DFLL48M_COARSE_CAL_ADDR) & FUSES_DFLL48M_COARSE_CAL_Msk) >> FUSES_DFLL48M_COARSE_CAL_Pos;
+
+SYSCTRL->DFLLVAL.bit.COARSE = coarse;
+
+/* Wait for the write to finish */
+while(!SYSCTRL->PCLKSR.bit.DFLLRDY);
+
+SYSCTRL->DFLLCTRL.reg |=
+    /* Closed loop mode */
+    SYSCTRL_DFLLCTRL_MODE |
+    /* Wait for the frequency to be locked before outputting the clock */
+    SYSCTRL_DFLLCTRL_WAITLOCK |
+    /* Enable it */
+    SYSCTRL_DFLLCTRL_ENABLE;
+
+/* Wait for the frequency to lock */
+while (!SYSCTRL->PCLKSR.bit.DFLLLCKC || !SYSCTRL->PCLKSR.bit.DFLLLCKF) {}
+
+/* Setup GCLK0 using the DFLL @ 48 MHz */
+GCLK->GENCTRL.reg =
+    GCLK_GENCTRL_ID(0) |
+    GCLK_GENCTRL_SRC_DFLL48M |
+    /* Improve the duty cycle. */
+    GCLK_GENCTRL_IDC |
+    GCLK_GENCTRL_GENEN;
+
+/* Wait for the write to complete */
+while(GCLK->STATUS.bit.SYNCBUSY);
+}
---
+++ MCU-GP-27-3-PD-temp/gp1z.h
@@ -0,0 +1,67 @@
+#ifndef GP1Z_H
+#define GP1Z_H
+
+#include "IMU_PIG_DEFINE.h"
+#include "wiring_private.h"
+#include "crcCalculator.h"
+#include <TinyGPSPlus.h>
+#include "myI2C.h"
+#include "mySPI.h"
+#include "myUART.h"
+#include "myPWM.h"
+#include "myWDT.h"
+
+typedef union
+{
+  float float_val;
+  uint8_t bin_val[4];
+  uint32_t ulong_val;
+}my_time_t;
+
+typedef union
+{
+  float float_val[3];
+  uint8_t bin_val[12];
+  int int_val[3];
+}my_acc_t;
+
+typedef union
+{
+  float float_val;
+  uint8_t bin_val[4];
+  int int_val;
+}my_float_t;
+
+#define PRINT_SELECT_FN(x) \
+  Serial.println("\nprint SELECT_FN"); \
+  if (x == 0) { \
+      Serial.println("SELECT SEL_DEFAULT"); \
+  } else if (x == 1) { \
+      Serial.println("SELECT SEL_RST"); \
+  } else if (x == 2) { \
+      Serial.println("SELECT SEL_FOG_1"); \
+  } else if (x == 3) { \
+      Serial.println("SELECT SEL_FOG_2"); \
+  } else if (x == 4) { \
+      Serial.println("SELECT SEL_FOG_3"); \
+  } else if (x == 5) { \
+      Serial.println("SELECT SEL_IMU"); \
+  } else if (x == 6) { \
+      Serial.println("SELECT SEL_NMEA"); \
+  } else if (x == 7) { \
+      Serial.println("SELECT SEL_FOG_PARA"); \
+  } else if (x == 8) { \
+      Serial.println("SELECT SEL_HP_TEST"); \
+  }\
+  else { \
+      Serial.println("Invalid input"); \
+  }
+
+#define PRINT_MUX_FLAG(x) \
+  Serial.println("\nprint mux_flag"); \
+  if(x == 0) Serial.println("MUX_OUTPUT"); \
+  else if(x == 1) Serial.println("MUX_PARAMETER"); \
+  else if(x == 2) Serial.println("MUX_ESCAPE"); 
+
+
+#endif
