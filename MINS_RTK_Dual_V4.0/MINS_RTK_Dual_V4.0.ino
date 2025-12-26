1deb5e4aa903b6928f86df74d70a07af28d7db6b
adamShiau <69100634+adamShiau@users.noreply.github.com>
2025-12-26 14:08:46 +0800

    d

--- MINS_RTK_Dual_V4.0/MINS_RTK_Dual_V4.0.ino
+++ MINS_RTK_Dual_V4.0/MINS_RTK_Dual_V4.0.ino
@@ -12,7 +12,7 @@
 NmeaParser gps_parser;
 
 void setup() {
-  set_system_clk(EXTERNAL_CLK);
+  set_system_clk(INTERNAL_CLK);
   pwm_init();
   pwm.timer(1, 2, int(60000), false); //12M/2/60000 = 100Hz
   pwm.analogWrite(PWM100, 500);  
